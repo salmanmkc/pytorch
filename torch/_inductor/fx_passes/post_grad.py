de97bf34414692410f2ec79321d16d6cb4da2925
Nikita Vedeneev <nvedzeneyeu@nvidia.com>
2025-11-03 16:15:50 +0000

    [Inductor] refine the logic in addmm -> mm + add
    ghstack-source-id: 7cf8fe4c28e7d3b790934c8a380fbb8771c787e8
    Pull-Request: https://github.com/pytorch/pytorch/pull/166170

--- torch/_inductor/fx_passes/post_grad.py
+++ torch/_inductor/fx_passes/post_grad.py
@@ -1505,15 +1505,45 @@ def view_to_reshape(gm):
         nd.target = torch.ops.aten.reshape.default
 
 
+# Relevant for addmm and (add + mm)/(mm + add)
+# Follows the dispatch logic for cuBLASLt at
+# aten/src/ATen/native/cuda/Blas.cpp::isInputCompliesAddmmCudaLt
+def _cublaslt_can_fuse_bias_epilogue(inp, mat1, mat2):
+    if config.max_autotune_gemm:
+        return False
+
+    # match the dispatch logic for cuBLASLT at aten/src/ATen/native/cuda/Blas.cpp
+    if not (
+        inp.is_cuda
+        and (inp.dim() == 1 or inp.squeeze().dim == 1)
+        and inp.is_contiguous()
+    ):
+        return False
+
+    if not (mat1.dim() == 2 and mat2.dim() == 2):
+        return False
+
+    if inp.size(0) != mat2.size(1):
+        return False
+
+    if inp.dtype != mat1.dtype or inp.dtype != mat2.dtype:
+        return False
+
+    return True
+
+
 def should_prefer_unfused_addmm(match):
     inp = match.kwargs["inp"]
     if not is_gpu(inp.meta["val"].device.type):
         return False
 
-    return has_uses_tagged_as(
-        match.output_node(),
-        (torch.Tag.pointwise, torch.Tag.reduction),
-    )
+    if has_uses_tagged_as(
+        match.output_node(), (torch.Tag.pointwise, torch.Tag.reduction)
+    ):
+        return True
+    else:
+        args_val = (arg.meta["val"] for arg in (inp, *match.args))
+        return not _cublaslt_can_fuse_bias_epilogue(*args_val)
 
 
 @register_graph_pattern(
